a9f0416d3c911eed9645a9e944dfc8361727e543
EhEhRon91 <65462273+EhEhRon91@users.noreply.github.com>
2021-09-18 23:47:12 -0400

    Now able to extract info to a .json file.

--- CustomBrightspaceDriver/Program.cs
+++ CustomBrightspaceDriver/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json.Linq;
 
 //~~~~~Custom imports
 using OpenQA.Selenium;
@@ -10,14 +12,14 @@ namespace CustomBrightspaceDriver
 {
 	class Program
 	{
-		private const string USER = "j_wootton79093";
-		private const string PASS = "";
+		private const string USER = "youruser";
+		private const string PASS = "yourpass";
 
 		private const string URL1 = "https://login.fanshawec.ca/cas/login?service=https%3A%2F%2Flogin.fanshawec.ca%2Fidp%2FAuthn%2FExtCas%3Fconversation%3De2s1&entityId=https%3A%2F%2Fc70baeb4-290e-4d0f-864b-dee68f276d2c.tenants.brightspace.com%2FsamlLogin";
 		private const string URL2 = "https://www.fanshaweonline.ca/";
-		private const string URL3 = "https://www.fanshaweonline.ca/d2l/api/lp/1.0/users/whoami";
+		private const string URL3 = "https://www.fanshaweonline.ca/d2l/api/lp/1.9/enrollments/myenrollments/";
 
-		static async System.Threading.Tasks.Task Main(string[] args)
+		static void Main(string[] args)
 		{
 			//IWebDriver driver = new ChromeDriver(AppDomain.CurrentDomain.BaseDirectory);
 
@@ -41,12 +43,22 @@ namespace CustomBrightspaceDriver
 
 			driver.FindElement(By.Name("submit")).Click();
 
-			//~~~ Navigate Who-am-i
+			//~~~ Navigate URL
 			driver.Navigate().GoToUrl(URL2);
 			driver.Navigate().GoToUrl(URL3);
 
-			string[] webrequests = {driver.PageSource};
-			await File.WriteAllLinesAsync("WebRequests.html", webrequests);
+			//string[] webrequests = {driver.PageSource};
+			//await File.WriteAllLinesAsync("WebRequests.html", webrequests);
+
+			string htmlAsText = driver.PageSource;
+			string jsonRegex = @"\{(?:[^\{\}]|(?<o>\{)|(?<-o>\}))+(?(o)(?!))\}";
+			string input = htmlAsText.Substring(htmlAsText.IndexOf("pre-wrap;\">"));
+
+			Match match = Regex.Matches(input, jsonRegex, RegexOptions.Multiline | RegexOptions.IgnoreCase)[0];
+
+			string jsonText = match.Groups[0].Value;
+			JObject jsonObj = JObject.Parse(jsonText);
+			File.WriteAllText("info.json", jsonObj.ToString());
 
 			driver.Quit();
 
